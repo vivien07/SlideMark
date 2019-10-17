017ad566cc6bacd82983ed9b48ed684040813b36
vivien ruas <asperger07@icloud.com>
2019-10-17 17:11:32 +0200

    Initial Commit

---
+++ SlideMark/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlideMark
+//
+//  Created by Administrator on 17/10/2019.
+//  Copyright © 2019 www.theiphonedeveloper.fr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ SlideMark/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SlideMark
+//
+//  Created by Administrator on 17/10/2019.
+//  Copyright © 2019 www.theiphonedeveloper.fr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
